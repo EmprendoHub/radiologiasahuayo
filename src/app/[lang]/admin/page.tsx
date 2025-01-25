f2d3018863630c2e904bf44e03560f8c024388b9
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2025-01-24 18:35:13 -0600

    minor update

--- src/app/[lang]/admin/page.tsx
+++ src/app/[lang]/admin/page.tsx
@@ -1,15 +1,5 @@
-import DashComponent from "./_components/DashComponent";
-import { getDashboard } from "../_actions";
-
-const AdminDashboardPage = async ({ params }: { params: { lang: string } }) => {
-  const lang = params.lang;
-  const data = await getDashboard(lang);
-
-  return (
-    <>
-      <DashComponent data={data} lang={lang} />
-    </>
-  );
+const AdminDashboardPage = async () => {
+  return <></>;
 };
 
 export default AdminDashboardPage;
